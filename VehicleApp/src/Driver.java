65165d90a28eef5ce09ebb653af20be2cb81dd7a
Xinjie-W1026 <202483910024@nuist.edu.cn>
2024-10-23 16:58:27 +0800

    Update Vehicle Application

--- VehicleApp/src/Driver.java
+++ VehicleApp/src/Driver.java
@@ -1,34 +1,53 @@
 import java.util.Scanner;
 
 public class Driver {
+
     Vehicle car;
-    Vehicle EBike;
-    Vehicle bus;
     Scanner input = new Scanner(System.in);
-    public static void main(String args[]){
+
+    public static void main(String[] args) {
         System.out.println("Vehicle App V1.0");
-        new Driver();
-
+        Driver driver = new Driver();
+        driver.addVehicle();
+        driver.printVehicle();
+        driver.checkBestVehicle();
     }
 
-    public Driver() {
-        car = new Vehicle(4, 20000, "BYD", "Song" ,4);
-        EBike = new Vehicle(2,200,"7ma","def",2);
-        System.out.println("The number of passengers: ");
-        int numpassengers = input.nextInt();
-
-    }
-    public void checkBestVehicle(){
-
-    }
     public void addVehicle() {
-        System.out.println("Enter Make: " );
+        System.out.print("Enter Make : ");
         String carMake = input.nextLine();
-        System.out.println("Enter Model: " );
+        System.out.print("Enter Model : ");
         String carModel = input.nextLine();
-        System.out.println("Enter Price: " );
+        System.out.print("Enter Price : ");
         float carPrice = input.nextFloat();
-        System.out.println("Enter Passengers: " );
+        System.out.print("Enter Passengers : ");
         int carPassengers = input.nextInt();
+        System.out.print("Enter Wheels : ");
+        int carWheels = input.nextInt();
+
+        car = new Vehicle(carMake, carModel, carPrice,
+                carPassengers,carWheels);
+
     }
+
+    public void printVehicle() {
+        System.out.println(car);
+    }
+
+    public void checkBestVehicle() {
+        System.out.print("Enter Number of Passengers : ");
+        int numPassengers = input.nextInt();
+        System.out.println("Passengers Entered : " + numPassengers);
+
+        if(numPassengers >= 1 && numPassengers <= 2)
+            System.out.println("EBike is Best");
+        else if(numPassengers >= 3 && numPassengers <= 5)
+            System.out.println("Car is Best : ");
+        else if(numPassengers >= 6 && numPassengers <= 20)
+            System.out.println("Bus is Best : ");
+        else
+            System.out.println("Sorry, no Vehicle Available");
+    }
+
 }
+
--- VehicleApp/src/Vehicle.java
+++ VehicleApp/src/Vehicle.java
@@ -1,72 +1,79 @@
 public class Vehicle {
-    private int passengers, numwheels;
-    private String make, model;
-    private float price;
-
-    public Vehicle(){
-        passengers = 5;
-        make = "BYD";
-        model = "QING";
-        price = 50000.0f;
-        numwheels = 4;
+    private String model = "ACME Model";
+    private String make = "ACME Make";
+    private float price = 10000;
+    private int passengers = 5;
+    private int numWheels = 5;
+    /* public Vehicle() {
+        this.passengers = 5;
+        this.price = 10000;
+        this.model = "ACME Model";
+        this.make = "ACME Make";
+        this.numWheels = 5;
     }
-    public Vehicle(int passengers, float price, String make, String model,int numwheels ) {
+     */
+    public Vehicle(String make, String model, float price,
+                   int passengers, int numWheels) {
         setPassengers(passengers);
-        setWheels(numwheels);
-        setMake(make);
-        setModel(model);
         setPrice(price);
+        setModel(model);
+        setMake(make);
+        setNumWheels(numWheels);
     }
-
     public int getPassengers() {
         return passengers;
     }
-
     public void setPassengers(int passengers) {
-        this.passengers = passengers;
-    }
-
-    public int getWheels() {
-        return numwheels;
-    }
-
-    public void setWheels(int wheels) {
-        this.numwheels = wheels;
-    }
-
-    public String getMake() {
-        return make;
-    }
-
-    public void setMake(String make) {
-        this.make = make;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
+        if(passengers >= 2 && passengers <= 8)
+            this.passengers = passengers;
+        else
+            System.out.println("Unable to set Passengers");
     }
     public float getPrice() {
         return price;
     }
     public void setPrice(float price) {
-        this.price = price;
+        if(price >= 100 && price <= 10000)
+            this.price = price;
+        else
+            System.out.println("Unable to set Price");
     }
-    public String toString(){
-        return "Vehicle details" + "\n" + "Passengers: " + passengers +
-                ",Make : " + make +
-                ",Model : " + model +
-                ",wheels : " + numwheels
-                +"\n";
+    public String getModel() {
+        return model;
     }
-    public void display(){
-        System.out.println("Vehicle details");
-        System.out.print("Passengers: " + passengers);
-        System.out.print(",Make : " + make);
-        System.out.print(",Model : " + getModel());
-        System.out.println(",wheels : " + getWheels());
+    public void setModel(String model) {
+        if(model != null)
+            if(model.length() > 0)
+                this.model = model;
+        else
+             System.out.println("Unable to set Model");
     }
-}
+    public String getMake() {
+        return make;
+    }
+    public void setMake(String make) {
+        if(make != null)
+            if(make.length() > 0)
+                this.make = make;
+            else
+                System.out.println("Unable to set Make");
+    }
+    public int getNumWheels() {
+        return numWheels;
+    }
+    public void setNumWheels(int numWheels) {
+        if(numWheels >= 2 && numWheels <= 20)
+            this.numWheels = numWheels;
+        else
+            System.out.println("Unable to set numWheels");
+    }
+    public String toString() {
+        return "Vehicle Details" + "\n" +
+                "Make : " + make + "," +
+                "Model : " + model + "," +
+                "Price : " + price + "," +
+                "Passengers : " + passengers + "," +
+                "Wheels : " + numWheels;
+    } // End of toString()
+} // End of Class Vehicle
+///////////////////////////////////////////////////////
